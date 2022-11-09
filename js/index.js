ad0381b9de1504516b5b5dc3991907ea52d3f784
Hassir Lastre Sierra <hassir.lastre@outlook.com>
2022-11-09 15:19:51 -0600

    Version 4

--- js/index.js
+++ js/index.js
@@ -29,6 +29,36 @@ window.addEventListener('scroll', function () {
   prevY = window.scrollY;
 });
 
+//Cerrar menú al seleccionar una opción
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute('id');
+      const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        document.querySelector('.menu a.selected').classList.remove('selected');
+        menuLink.classList.add('selected');
+      }
+    });
+  },
+  { rootMargin: '-30% 0px -70% 0px' }
+);
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', function () {
+    menu.classList.remove('menu_opened');
+  });
+
+  const hash = menuLink.getAttribute('href');
+  const target = document.querySelector(hash);
+  if (target) {
+    observer.observe(target);
+  }
+});
+
 //Efecto maquina de escribir
 
 const typed = new Typed('.typed', {
